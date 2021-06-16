1e392866f70f2127582ef791e1a9b14a268c16ab
Raul Osuna <raulakeem35@gmail.com>
2021-06-16 15:43:58 -0500

    Add custom traces in node

--- todos-api/todoController.js
+++ todos-api/todoController.js
@@ -13,10 +13,10 @@ class TodoController {
 
     // TODO: these methods are not concurrent-safe
     list (req, res) {
-        var self = this
-        const test = nr.startSegment('testing', true, function() {
+        var self = this;
+        const test = nr.startSegment('list-items', true, function() {
             const data = self._getTodoData(req.user.username)
-            return data.items
+            return data.items;
         })
         
         res.json(test)
@@ -25,72 +25,96 @@ class TodoController {
     create (req, res) {
         // TODO: must be transactional and protected for concurrent access, but
         // the purpose of the whole example app it's enough
-        const data = this._getTodoData(req.user.username)
-        const todo = {
-            content: req.body.content,
-            id: data.lastInsertedID
-        }
-        data.items[data.lastInsertedID] = todo
-        data.lastInsertedID++
-        this._setTodoData(req.user.username, data)
-        this._logOperation(OPERATION_CREATE, req.user.username, todo.id)
-        res.json(todo)
+        var self = this;
+
+        const result = nr.startSegment('create-item', true, function() {
+            const data = self._getTodoData(req.user.username)
+            const todo = {
+                content: req.body.content,
+                id: data.lastInsertedID
+            }
+            data.items[data.lastInsertedID] = todo
+            data.lastInsertedID++
+            self._setTodoData(req.user.Fusername, data)
+            self._logOperation(OPERATION_CREATE, req.user.username, todo.id)
+            return todo;
+        });
+        
+        res.json(result)
     }
 
     delete (req, res) {
+
+        var self = this;
         const data = this._getTodoData(req.user.username)
         const id = req.params.taskId
-        delete data.items[id]
-        this._setTodoData(req.user.username, data)
+
+        nr.startSegment('delete-item', true, function() {
+            delete data.items[id]
+            self._setTodoData(req.user.username, data)
+        });
+
         this._logOperation(OPERATION_DELETE, req.user.username, id)
         res.status(204)
         res.send()
     }
 
     _logOperation(opName, username, todoId) {
-      this._redisClient.publish(
-        this._logChannel,
-        JSON.stringify({
-          opName,
-          username,
-          todoId,
-        }),
-        function(err) {
-          if (err) {
-          }
-        }
-      )
+        var self = this;
+        nr.startSegment('logging-operation', true, function() {
+            self._redisClient.publish(
+                self._logChannel,
+                JSON.stringify({
+                    opName,
+                    username,
+                    todoId,
+                }),
+                function(err) {
+                    if (err) {}
+                }
+            )
+        }); 
     }
 
     _getTodoData (userID) {
-        var data = cache.get(userID)
-        if (data == null) {
-            data = {
-                items: {
-                    '1': {
-                        id: 1,
-                        content: "Create new todo",
+        var self = this;
+
+        var result = nr.startSegment('getting-items', true, function() {
+            var data = cache.get(userID)
+            if (data == null) {
+                data = {
+                    items: {
+                        '1': {
+                            id: 1,
+                            content: "Create new todo",
+                        },
+                        '2': {
+                            id: 2,
+                            content: "Update me",
+                        },
+                        '3': {
+                            id: 3,
+                            content: "Delete example ones",
+                        }
                     },
-                    '2': {
-                        id: 2,
-                        content: "Update me",
-                    },
-                    '3': {
-                        id: 3,
-                        content: "Delete example ones",
-                    }
-                },
-                lastInsertedID: 3
+                    lastInsertedID: 3
+                }
+
+                self._setTodoData(userID, data)
+                self._logOperation('GET', userID, data)
             }
 
-            this._setTodoData(userID, data)
-            this._logOperation('GET', userID, data)
-        }
-        return data
+            return data
+        });
+        
+        return result
     }
 
     _setTodoData (userID, data) {
-        cache.put(userID, data)
+        nr.startSegment('setting-items', true, function() {
+            cache.put(userID, data)
+        });
+
         this._logOperation('SET', userID, data)
     }
 }
